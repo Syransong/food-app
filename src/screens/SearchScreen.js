da62bf3f7a60a51189464eaa0bd9094770600b3d
Syransong <ksysong@gmail.com>
2021-07-14 15:49:11 -0700

    add props to Flatlist component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,9 @@ const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchAPI, results, errorMessage] = useResults();
   
+  const filterResultsByPrice = (price) => {
+    //price === "$" || "$$" || "$$$"
+  }
   return (
     <View>
       <SearchBar 
@@ -17,9 +20,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Cost Effective" />
-      <ResultsList title="Bit Pricier"/>
-      <ResultsList title="Big Spender!"/>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier"/>
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender!"/>
     </View>
   );
 };
