e45e03307342f8ffc6a4872d61735e9716f6db22
Syransong <ksysong@gmail.com>
2021-07-15 22:11:16 -0700

    add styling to results detail and results list

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,22 +3,26 @@ import { View, Image, Text, StyleSheet } from "react-native";
 
 const ResultsDetail = ({ result }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image 
         style={styles.image}
         source={{ uri: result.image_url }}
       />
       <Text style={styles.name}>{result.name}</Text>
-      <Text>{result.rating} Stas, {result.review_count} Reviews</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15
+  },
   image: {
     width: 250,
     height: 120,
-    borderRadius: 4
+    borderRadius: 4,
+    marginBottom: 5
   },
   name: {
     fontWeight: "bold",
